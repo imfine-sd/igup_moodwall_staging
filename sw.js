f85a177945f2d2748386187fed879e046259e5fd
imfine-sd <53457765+imfine-sd@users.noreply.github.com>
2023-06-01 16:33:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,37 @@
 // install event
 self.addEventListener("install", (event) => {
-  // console.log("[Service Worker] installed");
+  console.log("[Service Worker] installed");
+
+  event.waitUntil(
+    caches.open("moodwall").then(function (cache) {
+      return cache.addAll(["./", "./index.html", "./fonts/", "./icons/"]);
+    })
+  );
 });
 
 // activate event
 self.addEventListener("activate", (event) => {
-  // console.log("[Service Worker] actived", event);
+  console.log("[Service Worker] actived", event);
 });
 
 // fetch event
 self.addEventListener("fetch", (event) => {
-  // console.log("[Service Worker] fetched resource " + e.request.url);
+  console.log("[Service Worker] fetched resource " + event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      console.log("[Service Worker] Fetching resource: " + event.request.url);
+      return (
+        response ||
+        fetch(event.request).then(function (response) {
+          return caches.open("danbi").then(function (cache) {
+            console.log(
+              "[Service Worker] Caching new resource: " + event.request.url
+            );
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
 });
